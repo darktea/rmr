dcd5d0cfc8c5227fc5f498e052055e66fd23a2db
darktea <tiantianding@gmail.com>
2022-04-07 11:48:47 +0800

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use std::collections::HashMap;
+use reqwest;
+use reqwest::header;
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
+    tracing_subscriber::fmt::init();
+    let mut headers = header::HeaderMap::new();
+    headers.insert("Accept", header::HeaderValue::from_static("text/plain"));
+
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .timeout(Duration::from_secs(3))
+        .connection_verbose(true)
+        .build()?;
+
+    let doge = client
+        .get("https://httpbin.org/ip")
+        .send()
         .await?
-        .json::<HashMap<String, String>>()
+        .text()
         .await?;
-    println!("Got {:#?}", resp);
+
+    println!("Got {:#?}", doge);
     Ok(())
 }
